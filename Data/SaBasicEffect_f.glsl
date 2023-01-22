d4fb2510ccd8d5da3943c7318327830e0ce1556e
Darrell Blake <darrell@cobbly.co.uk>
2023-01-22 16:26:37 +0000

    Removed unnecessary files.

--- Data/SaBasicEffect_f.glsl
+++
@@ -1,9 +0,0 @@
-uniform sampler2D texture;
-
-varying vec4 vColour;
-varying vec2 vTexCoord;
-
-void main()
-{
-    gl_FragColor = texture2D(texture, vTexCoord) * vColour;
-}
--- Data/SaSpriteEffect_f.glsl
+++
@@ -1,9 +0,0 @@
-uniform sampler2D texture;
-uniform vec4 colour;
-
-varying vec2 vTexCoord;
-
-void main()
-{
-    gl_FragColor = texture2D(texture, vTexCoord) * colour;
-}
